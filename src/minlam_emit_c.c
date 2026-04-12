16e1b30aa5286dd0c0715d362bd438cdba4d7dde
Bill Hails <billhails2014@gmail.com>
2026-04-12 13:37:26 +0100

    checkpoint

--- src/minlam_emit_c.c
+++ src/minlam_emit_c.c
@@ -20,3 +20,53 @@
  */
 
 #include "minlam_emit.inc"
+
+//////////////
+// Public API
+//////////////
+
+void emitCProgram(MinExp *node, BuiltIns *builtIns, FILE *out) {
+    Opaque *body = newOpaque_EmitBuffer();
+    int save = PROTECT(body);
+    HashSymbol *main = newSymbol("main");
+    SymbolArray *heap = emitter_createHeap();
+    PROTECT(heap);
+    EmitterContext context = newEmitterContext(main, builtIns, heap);
+    PROTECT(context.slots);
+    CEmitterContext *ctx = newCEmitterContext(body, context);
+    REPLACE_PROTECT(save, ctx);
+    fprintf(out, "// GENERATED CODE DO NOT EDIT\n");
+    fprintf(out, "#include \"minlam_runtime.h\"\n");
+    fprintf(out, "extern int forceGcFlag;\n");
+    fprintf(out, "#ifdef TRACE_GOTO\n");
+    fprintf(out, "#  define TRACE(from, target, nargs) "
+                 "fprintf(stderr, \"%%s -> %%p (%%d args)\\n\", (from), (void "
+                 "*)(target), (nargs))\n");
+    fprintf(out, "#else\n");
+    fprintf(out, "#  define TRACE(from, target, nargs)\n");
+    fprintf(out, "#endif\n");
+    emitMinExp(node, ctx);
+    fprintf(out, "#define MAX_REG %d\n", ctx->context.maxReg);
+    fprintf(out, "static Value reg[MAX_REG];\n");
+    fprintf(out, "\n");
+
+    fprintf(out, "int main(int argc, char *argv[]) {\n");
+    fprintf(out, "for (int i = 0; i < MAX_REG; i++) {\n");
+    fprintf(out, "reg[i] = value_None();\n");
+    fprintf(out, "}\n");
+    fprintf(out, "minlam_runtime_init(reg, MAX_REG, argc, argv);\n");
+    fprintf(out, "// forceGcFlag = 1;\n");
+    fprintf(out, "goto ENTRY;\n");
+    Index i = 0;
+    HashSymbol *label = NULL;
+    Opaque *buf = NULL;
+    i = 0;
+    while ((label = iterateCBufferBag(ctx->lambdas, &i, &buf)) != NULL) {
+        SCharArray *l = makeLambdaLabel(label);
+        fprintf(out, "%s:\n", l->entries);
+        fprintf(out, "%s\n", opaqueEmitBufferContent(buf));
+    }
+    fprintf(out, "\nENTRY:\n");
+    fprintf(out, "%s\n", opaqueEmitBufferContent(ctx->body));
+    fprintf(out, "}\n");
+}
